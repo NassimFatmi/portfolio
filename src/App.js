5b7da6437fb61fb0e0f91a52fcd439df6f72c9be
Nassim Fatmi <in_fatmi@esi.dz>
2023-02-24 22:04:41 +0100

    remove routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-} from "react-router-dom";
-import { MainView, FlutterView } from "./pages";
+import { MainView } from "./pages";
 import "animate.css";
 
 const App = () => {
 	return (
-		<Router>
-			<div className="app bg-primary">
-				<Switch>
-					<Route path="/" exact>
-						<MainView />
-					</Route>
-					<Route path="/flutter">
+		// <Router>
+		<div className="app bg-primary">
+			{/* <Switch> */}
+			{/* <Route path="/" exact> */}
+			<MainView />
+			{/* </Route> */}
+			{/* <Route path="/flutter">
 						<FlutterView />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
+					</Route> */}
+			{/* </Switch> */}
+		</div>
+		// </Router>
 	);
 };
 
